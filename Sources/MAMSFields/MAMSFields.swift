8f5edae30f4bfeb1138f833bc650c9a3205e3bca
mmuellersoppart <mmuellersoppart@gmail.com>
2022-05-07 10:07:45 -0500

    package dependency no longer in local

--- Sources/MAMSFields/MAMSFields.swift
+++
@@ -1,6 +0,0 @@
-public struct MAMSFields {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
--- Tests/MAMSFieldsTests/MAMSFieldsTests.swift
+++ Tests/MAMSFieldsTests/MAMSFieldsTests.swift
@@ -6,6 +6,6 @@ final class MAMSFieldsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MAMSFields().text, "Hello, World!")
+//        XCTAssertEqual(MAMSFields().text, "Hello, World!")
     }
 }
