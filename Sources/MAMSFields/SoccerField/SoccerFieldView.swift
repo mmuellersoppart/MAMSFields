d948727c80514b418a99cb26cbc9d1846f29bddb
mmuellersoppart <mmuellersoppart@gmail.com>
2022-05-08 10:34:41 -0500

    allow for manually set scale

--- Sources/MAMSFields/SoccerField/SoccerFieldView.swift
+++ Sources/MAMSFields/SoccerField/SoccerFieldView.swift
@@ -13,8 +13,11 @@ public struct SoccerFieldView: View {
     var strokeColor: Color = Color.white
     var strokeWidth: Double = 1.0
     var fillColor: Color = Color.green
+    var scale: Double?
     
-    public init(radians: Double = 0, strokeColor: Color = Color.white, fillColor: Color = Color.green, strokeWidth: Double = 1) {
+    public init(radians: Double = 0, strokeColor: Color = Color.white, fillColor: Color = Color.green, strokeWidth: Double = 1, scale: Double? = nil) {
+
+        self.scale = scale
         self.radians = radians
         self.strokeColor = strokeColor
         self.fillColor = fillColor
@@ -26,7 +29,8 @@ public struct SoccerFieldView: View {
         Canvas { context, size in
             
             // find the scale that maximized field in boundary
-            let scaleToFit = Field.determineFieldScale(totalFieldSize: Size2D(x: SoccerFieldConstants.totalW, y: SoccerFieldConstants.totalH), boundarySize: size.asSize2D, rotation: radians)
+            
+            let scaleToFit = scale ?? Field.determineFieldScale(totalFieldSize: Size2D(x: SoccerFieldConstants.totalW, y: SoccerFieldConstants.totalH), boundarySize: size.asSize2D, rotation: radians)
             
             let soccerField = SoccerField(scale: scaleToFit, radians: radians, centerPoint: Point2D(x: size.width/2, y: size.height/2))
             
@@ -84,6 +88,6 @@ public struct SoccerFieldView: View {
 
 struct SoccerFieldView_Previews: PreviewProvider {
     static var previews: some View {
-        SoccerFieldView(radians: Double.pi/2)
+        SoccerFieldView(radians: Double.pi/2, strokeWidth: 0.6)
     }
 }
