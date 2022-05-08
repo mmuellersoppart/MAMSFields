de2067fe89ae820ec9d959ba8dd40d3a8999fd2d
mmuellersoppart <mmuellersoppart@gmail.com>
2022-05-08 10:18:51 -0500

    reduce soccer field view to all thats necessary

--- Sources/MAMSFields/SoccerField/SoccerFieldView.swift
+++ Sources/MAMSFields/SoccerField/SoccerFieldView.swift
@@ -22,66 +22,62 @@ public struct SoccerFieldView: View {
     }
     
     public var body: some View {
-        ZStack {
-            Color.gray
-            Canvas { context, size in
-
-                // find the scale that maximized field in boundary
-//                let scaleToFit = determineSoccerFieldScale(boundaryH: size.height, boundaryW: size.width, rotation: radians)
-                let scaleToFit = determineSoccerFieldScale(totalFieldSize: Size2D(x: SoccerFieldConstants.totalW, y: SoccerFieldConstants.totalH), boundarySize: size.asSize2D, rotation: radians)
-                
-                let soccerField = SoccerField(scale: scaleToFit, radians: radians, centerPoint: Point2D(x: size.width/2, y: size.height/2))
-                
-                // field
-                let totalFieldPath = soccerField.totalField
-                
-                context.fill(totalFieldPath, with: .color(.clear))
-                
-                let fieldPath = soccerField.field
-                
-                context.fill(fieldPath, with: .color(fillColor))
-                context.stroke(fieldPath, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                // midline
-                let midlinePath = soccerField.midline
-                context.stroke(midlinePath, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                // penalty boxes
-                // ********************
-                let penaltyBoxCirclePaths = soccerField.penaltyCircles
-                context.stroke(penaltyBoxCirclePaths, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                let penaltyBoxesPaths = soccerField.penaltyBoxes
-                context.fill(penaltyBoxesPaths, with: .color(fillColor))
-                context.stroke(penaltyBoxesPaths, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                let penaltyDotPaths = soccerField.penaltyDots
-                context.fill(penaltyDotPaths, with: .color(strokeColor))
-                
-                // goals
-                // ********************
-                let goalPaths = soccerField.goals
-                context.fill(goalPaths, with: .color(fillColor))
-                context.stroke(goalPaths, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                // goalie box
-                let goalieBoxPaths = soccerField.goalieBoxes
-                context.stroke(goalieBoxPaths, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                
-                // center circle
-                // ********************
-                let centerCirclePath = soccerField.centerCircle
-                context.stroke(centerCirclePath, with: .color(strokeColor), lineWidth: strokeWidth)
-                
-                //center dot
-                let centerDotPath = soccerField.centerDot
-                context.fill(centerDotPath, with: .color(strokeColor))
-                
-                //TODO: corner
-                let cornerPaths = soccerField.corners
-                context.stroke(cornerPaths, with: .color(strokeColor), lineWidth: strokeWidth)
-            }
+        
+        Canvas { context, size in
+            
+            // find the scale that maximized field in boundary
+            let scaleToFit = Field.determineFieldScale(totalFieldSize: Size2D(x: SoccerFieldConstants.totalW, y: SoccerFieldConstants.totalH), boundarySize: size.asSize2D, rotation: radians)
+            
+            let soccerField = SoccerField(scale: scaleToFit, radians: radians, centerPoint: Point2D(x: size.width/2, y: size.height/2))
+            
+            // field
+            let totalFieldPath = soccerField.totalField
+            
+            context.fill(totalFieldPath, with: .color(.clear))
+            
+            let fieldPath = soccerField.field
+            
+            context.fill(fieldPath, with: .color(fillColor))
+            context.stroke(fieldPath, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            // midline
+            let midlinePath = soccerField.midline
+            context.stroke(midlinePath, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            // penalty boxes
+            // ********************
+            let penaltyBoxCirclePaths = soccerField.penaltyCircles
+            context.stroke(penaltyBoxCirclePaths, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            let penaltyBoxesPaths = soccerField.penaltyBoxes
+            context.fill(penaltyBoxesPaths, with: .color(fillColor))
+            context.stroke(penaltyBoxesPaths, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            let penaltyDotPaths = soccerField.penaltyDots
+            context.fill(penaltyDotPaths, with: .color(strokeColor))
+            
+            // goals
+            // ********************
+            let goalPaths = soccerField.goals
+            context.fill(goalPaths, with: .color(fillColor))
+            context.stroke(goalPaths, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            // goalie box
+            let goalieBoxPaths = soccerField.goalieBoxes
+            context.stroke(goalieBoxPaths, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            
+            // center circle
+            // ********************
+            let centerCirclePath = soccerField.centerCircle
+            context.stroke(centerCirclePath, with: .color(strokeColor), lineWidth: strokeWidth)
+            
+            //center dot
+            let centerDotPath = soccerField.centerDot
+            context.fill(centerDotPath, with: .color(strokeColor))
+            
+            let cornerPaths = soccerField.corners
+            context.stroke(cornerPaths, with: .color(strokeColor), lineWidth: strokeWidth)
         }
     }
 }
@@ -90,17 +86,4 @@ struct SoccerFieldView_Previews: PreviewProvider {
     static var previews: some View {
         SoccerFieldView(radians: Double.pi/2)
     }
-}
-
-func determineSoccerFieldScale(totalFieldSize: Size2D, boundarySize: Size2D, rotation: Double) -> Double {
-    let centerPoint = Point2D(x: boundarySize.x/2, y: boundarySize.y/2)
-    let q2PosVector = PositionalVector2D(point: centerPoint, vector: Vector2D(x: totalFieldSize.x/2, y: totalFieldSize.y/2)).copy(radians: rotation)
-    let q3PosVector = PositionalVector2D(point: centerPoint, vector: Vector2D(x: totalFieldSize.x/2, y: -totalFieldSize.y/2)).copy(radians: rotation)
-    
-    let q2Intersection = findClosestIntersection(positionalVector: q2PosVector, xBoundaries: 0.0, boundarySize.x, yBoundaries: 0.0, boundarySize.y)
-    let q3Intersection = findClosestIntersection(positionalVector: q3PosVector, xBoundaries: 0.0, boundarySize.x, yBoundaries: 0.0, boundarySize.y)
-    
-    let closestIntersection = centerPoint.distance(to: q2Intersection!) > centerPoint.distance(to: q3Intersection!) ? q3Intersection : q2Intersection
-
-    return centerPoint.distance(to: closestIntersection!) / centerPoint.distance(to: q2PosVector.tip)
 }
